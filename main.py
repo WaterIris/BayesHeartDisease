50610d34bcb785e7366c699e1b7a23658a83f839
WaterIris <179727984+WaterIris@users.noreply.github.com>
2024-12-11 01:20:57 +0100

    add proper data and basic visualization

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('QtAgg')
 
-df_health = pd.read_csv("./health.csv")
+pd.set_option('display.precision', 2)
+# pd.set_option('display.max_columns', None)  # or 1000
+pd.set_option('display.max_rows', 20)  # or 1000
+# pd.set_option('display.max_colwidth', None)  # or 199
+heart = pd.read_csv("./data/heart.csv")
 
-print(df_health.info())
 
+print(f"{list(heart.columns)}\n")
+print(heart.info())
+# print(heart_df.describe(include="all"))
+print(heart.describe())
 
+features = heart.iloc[:, :-1]
+labels = heart.iloc[:, -1:]
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.hist(heart["age"])
+ax.set_xticks(np.arange(20, 81, 10))
+
+plt.show()
